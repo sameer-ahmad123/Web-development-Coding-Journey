b345f4bc23f27705fc5baf199d0431f2933f3ab0
sameer-ahmad123 <sameerahmad33245@gmail.com>
2026-05-18 18:53:09 +0500

    Added tutorial 112 and 113

--- tutorial113/src/App.jsx
+++ tutorial113/src/App.jsx
@@ -1,122 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+import {useState} from 'react'
+const App = () => {
+const [matchStats, setmatchStats] = useState({ runs: 0, wickets: 0, balls: 0 })
+const handleSingle = ()=>{
+  setmatchStats({
+    ...matchStats,runs:matchStats.runs +1,balls:matchStats.balls +1
+   
+  })
+  
+}
+const handleFour = ()=>{
+  setmatchStats({
+    ...matchStats,runs:matchStats.runs +4,balls:matchStats.balls +1
+   
+  })
+  
+}
+const handleDot = ()=>{
+  setmatchStats({
+    ...matchStats,balls:matchStats.balls +1
+   
+  })
+  
+}
+const handleWicket = ()=>{
+  setmatchStats({
+    ...matchStats,wickets:matchStats.wickets +1,balls:matchStats.balls +1
+   
+  })
+  
 }
 
+  return (
+    <div className='h-screen w-screen '>
+<nav className='h-24 w-full bg-amber-800 flex justify-center items-center'>
+  <ul className='flex gap-32 text-white'>
+    <li className='font-extrabold text-5xl'>Runs : {matchStats.runs}</li>
+    <li className='font-extrabold text-5xl'>Wickets : {matchStats.wickets}</li>
+    <li className='font-extrabold text-5xl'>Balls : {matchStats.balls}</li>
+  </ul>
+</nav>
+<div className='h-24 w-full text-white bg-cyan-950 flex justify-center items-center gap-10'>
+  
+  <button onClick={handleSingle} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
+    Add +1
+  </button>
+  
+  <button onClick={handleFour} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
+    Add +4
+  </button>
+  
+  <button onClick={handleDot} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
+    Dot Ball
+  </button>
+  
+  <button onClick={handleWicket} className='bg-red-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-red-700 transition-all hover:scale-105'>
+    Wicket!
+  </button>
+
+</div>
+    </div>
+  )
+}
 export default App
