a0114a87c15a5587a9715ab6ec8b8f7cba2ef1dd
sameer-ahmad123 <sameerahmad33245@gmail.com>
2026-05-19 18:25:22 +0500

    added tutorial 115

--- tutorial113/src/App.jsx
+++ tutorial113/src/App.jsx
@@ -1,64 +1,27 @@
 import {useState} from 'react'
+
 const App = () => {
-const [matchStats, setmatchStats] = useState({ runs: 0, wickets: 0, balls: 0 })
-const handleSingle = ()=>{
-  setmatchStats({
-    ...matchStats,runs:matchStats.runs +1,balls:matchStats.balls +1
-   
-  })
-  
-}
-const handleFour = ()=>{
-  setmatchStats({
-    ...matchStats,runs:matchStats.runs +4,balls:matchStats.balls +1
-   
-  })
-  
-}
-const handleDot = ()=>{
-  setmatchStats({
-    ...matchStats,balls:matchStats.balls +1
-   
-  })
-  
-}
-const handleWicket = ()=>{
-  setmatchStats({
-    ...matchStats,wickets:matchStats.wickets +1,balls:matchStats.balls +1
-   
-  })
-  
-}
+const [exam, setExam] = useState({ 
+  subject: "Database Management", 
+  targetMarks: 85, 
+  currentPrep: 50 
+})
 
   return (
-    <div className='h-screen w-screen '>
-<nav className='h-24 w-full bg-amber-800 flex justify-center items-center'>
-  <ul className='flex gap-32 text-white'>
-    <li className='font-extrabold text-5xl'>Runs : {matchStats.runs}</li>
-    <li className='font-extrabold text-5xl'>Wickets : {matchStats.wickets}</li>
-    <li className='font-extrabold text-5xl'>Balls : {matchStats.balls}</li>
-  </ul>
-</nav>
-<div className='h-24 w-full text-white bg-cyan-950 flex justify-center items-center gap-10'>
-  
-  <button onClick={handleSingle} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
-    Add +1
-  </button>
-  
-  <button onClick={handleFour} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
-    Add +4
-  </button>
-  
-  <button onClick={handleDot} className='bg-violet-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-violet-700 transition-all hover:scale-105'>
-    Dot Ball
-  </button>
-  
-  <button onClick={handleWicket} className='bg-red-600 border border-black rounded-xl px-6 py-2 font-bold hover:bg-red-700 transition-all hover:scale-105'>
-    Wicket!
-  </button>
-
-</div>
+    <div className='flex flex-col justify-center items-center w-screen h-screen'>
+      <div className="card border border-black">
+        <ul>
+        <li>Subject : {exam.subject}</li>
+        <li>Target Marks : {exam.targetMarks}</li>
+        <li>Current Prep : {exam.currentPrep}</li>
+        </ul>
+      </div>
+      <div >Enter Target Marks</div>
+        <input onChange={(e)=>setExam({...exam ,targetMarks : e.target.value })} className='border border-black' placeholder='Update Target Marks' type="text" />
+        <div>Enter current prep</div>
+        <input  onChange={(e)=>setExam({...exam ,currentPrep : e.target.value })} className='border border-black' placeholder='Update current Prep' type="text" />
     </div>
   )
 }
+
 export default App
