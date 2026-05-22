19b1947cc066d7d1423e3011ad7761169c7c9a0c
sameer-ahmad123 <sameerahmad33245@gmail.com>
2026-05-22 16:33:31 +0500

    practise of router

---
+++ tutorial115/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div  className='h-screen w-screen flex justify-center items-center bg-slate-400'>
+      <div className='text-9xl'>
+        Welcome to dashborad
+      </div>
+    </div>
+  )
+}
+
+export default Home
