e12df4a8e287a89120c53059481589a00857424b
sameer-ahmad123 <sameerahmad33245@gmail.com>
2026-05-23 07:41:53 +0500

    added tutorial116

--- tutorial115/src/App.jsx
+++ tutorial115/src/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
-import Login from './components/Login'
-import Home from './components/Home'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 const App = () => {
-  const router = createBrowserRouter ([
-    {
-      path : '/',
-      element : <Login/>
-    },
-    {
-      path : '/home',
-      element : <Home/>
-    }
-  ])
   return (
     <div>
-      <RouterProvider router={router}/>
+      
     </div>
   )
 }
--- tutorial115/src/components/Home.jsx
+++ tutorial115/src/components/Home.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div  className='h-screen w-screen flex justify-center items-center bg-slate-400'>
-      <div className='text-9xl'>
-        Welcome to dashborad
-      </div>
+    <div>
+        
     </div>
   )
 }
